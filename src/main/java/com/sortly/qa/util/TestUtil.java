2d3294b8cb33c2d7f976dc3709f28aa2472665a4
JayantaAutomation <jayantapanda.tech@gmail.com>
2024-03-30 08:09:08 +0530

    updated test data file and path, test data code i.e.TestUtil.java code

--- src/main/java/com/sortly/qa/util/TestUtil.java
+++ src/main/java/com/sortly/qa/util/TestUtil.java
@@ -27,8 +27,8 @@ public class TestUtil extends BaseTest {
 	public static long PAGE_LOAD_TIMEOUT = 20;
 	public static long IMPLICIT_WAIT = 20;
 
-	public static String TESTDATA_SHEET_PATH = "/Users/jayantakumarpanda/eclipse-workspace/AddressesCRUDOnSortlyUIAutomation"
-			+ "/src/main/java/com/sortly/qa/testdata/SortlyNewAddressData.xlsx";
+	public static String TESTDATA_SHEET_PATH = "C:\\Users\\Admin\\eclipse-workspace\\POMUIAutomation\\src\\main\\java\\com\\"
+			+ "sortly\\qa\\testdata\\SortlyNewAddressData.xlsx";
 
 	static Workbook book;
 	static Sheet sheet;
@@ -37,8 +37,9 @@ public class TestUtil extends BaseTest {
 	public void switchToFrame() {
 		driver.switchTo().frame("mainpanel");
 	}
-
-	public static Object[][] getTestData(String sheetName) throws InvalidFormatException {
+/*
+	public static Object[][] getTestData(String sheetName) throws InvalidFormatException 
+	{
 		FileInputStream file = null;
 		try {
 			file = new FileInputStream(TESTDATA_SHEET_PATH);
@@ -71,8 +72,38 @@ public class TestUtil extends BaseTest {
 		}
 		return data;
 	}
+*/
 
-
+	public static Object[][] getTestData(String sheetName) {
+		FileInputStream file = null;
+		try {
+			file = new FileInputStream(TESTDATA_SHEET_PATH);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		try {
+			book = WorkbookFactory.create(file);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		sheet = book.getSheet(sheetName);
+		Object[][] data = new Object[sheet.getLastRowNum()][sheet.getRow(0).getLastCellNum()];
+		// System.out.println(sheet.getLastRowNum() + "--------" +
+		// sheet.getRow(0).getLastCellNum());
+		for (int i = 0; i < sheet.getLastRowNum(); i++) {
+			for (int k = 0; k < sheet.getRow(0).getLastCellNum(); k++) {
+				if (k == 5) {
+					String zip = sheet.getRow(i + 1).getCell(k).toString();
+					data[i][k] = zip.substring(0,zip.indexOf('.'));
+				}else
+				{
+				data[i][k] = sheet.getRow(i + 1).getCell(k).toString();
+				}
+				// System.out.println(data[i][k]);
+			}
+		}
+		return data;
+	}	
 
 	public static void takeScreenshotAtEndOfTest() throws IOException {
 		File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
